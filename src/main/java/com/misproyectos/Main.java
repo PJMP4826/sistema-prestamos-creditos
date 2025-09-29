54573947190e3a3d07478401193977fd91da15eb
AdolfoRes01 <residente01@intelisof.com.mx>
2025-09-28 23:55:29 -0600

    test(database): agrega prueba para conexion a la bd en el main

--- src/main/java/com/misproyectos/Main.java
+++ src/main/java/com/misproyectos/Main.java
@@ -4,13 +4,26 @@ import com.misproyectos.config.Database;
 import com.misproyectos.models.Prestamo;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public class Main {
     public static void main(String[] args) {
+//        Prestamo prestamo = Prestamo.builder()
+//                .setIdPrestamo(123)
+//                .build();
+//
+//        System.out.println("Prestamo: " + prestamo.toString());
+//        System.out.println("Prestamo id: " + prestamo.getIdPrestamo());
 
+        try {
+            Connection db = Database.getInstance().getConnection();
+            System.out.println("Conexion exitosa: " + db);
+
+        } catch (Exception e) {
+            System.out.println("Conexion faild: ");
+            e.printStackTrace();
+
+        }
 
     }
 }
