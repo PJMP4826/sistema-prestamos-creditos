a1c38e53fbba77fc365bce55a78ca8f965fd5544
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-09-29 17:16:54 -0600

    feat(models): modelo de DireccionCliente agregado

---
+++ src/main/java/com/misproyectos/models/DireccionCliente.java
@@ -0,0 +1,35 @@
+package com.misproyectos.models;
+
+public class DireccionCliente {
+    private  Long idDireccion;
+    private  Long idCliente;
+    private  String tipo;
+
+    public DireccionCliente(){
+
+    }
+
+    public Long getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(Long idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
