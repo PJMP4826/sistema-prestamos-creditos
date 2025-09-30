32dbe7a20a0a496f2044df4f60cfa5a6c908d253
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-09-29 21:00:51 -0600

    feat(models): modelo de CorreoCliente agregado

---
+++ src/main/java/com/misproyectos/models/CorreoCliente.java
@@ -0,0 +1,35 @@
+package com.misproyectos.models;
+
+public class CorreoCliente {
+    private Long idCorreo;
+    private Long idCliente;
+    private String correo;
+
+    public CorreoCliente() {
+
+    }
+
+    public Long getIdCorreo() {
+        return idCorreo;
+    }
+
+    public void setIdCorreo(Long idCorreo) {
+        this.idCorreo = idCorreo;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
