6b30d8956a398eec43796251d752980bc09d3bd1
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-10-25 20:47:02 -0600

    feat(repository): agrega interface para repository de Correos de clientes

---
+++ src/main/java/com/misproyectos/interfaces/CorreoRepInterface.java
@@ -0,0 +1,18 @@
+package com.misproyectos.interfaces;
+
+import com.misproyectos.models.CorreoCliente;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CorreoRepInterface {
+    boolean add(CorreoCliente correoCliente) throws SQLException;
+
+    boolean update(CorreoCliente correoCliente) throws SQLException;
+
+    boolean delete(int idTelefono) throws SQLException;
+
+    CorreoCliente findById(int idCorreo) throws SQLException;
+
+    List<CorreoCliente> findByClientId(int idCorreo) throws SQLException;
+}
