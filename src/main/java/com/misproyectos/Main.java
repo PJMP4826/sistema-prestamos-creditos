9d03141f59159df091e814dbdad8c4a0f40849bc
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-11-02 23:31:06 -0600

    fix(main): se agrega la referencia al LookAndFeel del Swing en el main

--- src/main/java/com/misproyectos/Main.java
+++ src/main/java/com/misproyectos/Main.java
@@ -4,9 +4,22 @@ import com.misproyectos.views.MainWindow;
 
 import javax.swing.*;
 
+import static com.misproyectos.views.MainWindow.logger;
+
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
         java.awt.EventQueue.invokeLater(() -> {
             MainWindow window = new MainWindow();
             window.setTitle("Sistema de Prestamos");
