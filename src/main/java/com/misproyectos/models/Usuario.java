6e0cb81eb63301e2933b8d044539d27c60320505
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-11-04 10:43:15 -0600

    feat(models): add modelo de Usuario

---
+++ src/main/java/com/misproyectos/models/Usuario.java
@@ -0,0 +1,44 @@
+package com.misproyectos.models;
+
+public class Usuario {
+    private Long idUsuario;
+    private String nombreUsuario;
+    private String email;
+    private String password;
+
+    public Usuario() {
+        
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
